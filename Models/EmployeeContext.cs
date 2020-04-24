a75cb8dbbd1b060091139d26068edbb538db7628
Nagesh <nageshyour@gmail.com>
2020-04-24 07:55:36 +0000

    changes done DB

--- Models/EmployeeContext.cs
+++ Models/EmployeeContext.cs
@@ -1,8 +1,10 @@
+using System.Data;
 using System.IO;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.Extensions.Configuration;
+using Snowflake.Data.Client;
 
 namespace TestWebApi.Models
 {
@@ -15,6 +17,40 @@ namespace TestWebApi.Models
 
         var configuration = builder.Build();
         optionsBuilder.UseSqlServer(configuration["ConnectioinStrings:DeefaultConnnection"]);
+
+
+
+using (IDbConnection conn = new SnowflakeDbConnection())
+{
+    conn.ConnectionString = "Server=.;Database=mydb;user id=sa;password=123456";
+    conn.Open();
+
+    IDbCommand cmd = conn.CreateCommand();
+    cmd.CommandText = "insert into t values (?),(?),(?)";
+    IDataReader reader = cmd.ExecuteReader();
+                  
+    var p1 = cmd.CreateParameter();
+    p1.ParameterName = "1";
+    p1.Value = 10;
+    p1.DbType = DbType.Int32;
+    cmd.Parameters.Add(p1);
+
+    var p2 = cmd.CreateParameter();
+    p2.ParameterName = "2";
+    p2.Value = 10000L;
+    p2.DbType = DbType.Int32;
+    cmd.Parameters.Add(p2);
+
+    var p3 = cmd.CreateParameter();
+    p3.ParameterName = "3";
+    p3.Value = (short)1;
+    p3.DbType = DbType.Int16;
+    cmd.Parameters.Add(p3);
+
+    var count = cmd.ExecuteNonQuery();
+    
+    conn.Close();
+}
     }
 
     public DbSet<Employee> Employees { get; set; }
@@ -24,5 +60,8 @@ namespace TestWebApi.Models
 
     //         modelBuilder.Entity<Employee>().ToTable("Employee");        
     // }
+
+
+    
 }  
 }
